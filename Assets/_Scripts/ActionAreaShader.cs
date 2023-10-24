84281b78c7a20585dab1c243374a2ff611eb1adb
MirCore <9919366+MirCore@users.noreply.github.com>
2023-10-24 11:03:55 +0200

    Rename and move ActionAreaManager

---
+++ Assets/_Scripts/ActionAreaShader.cs
@@ -0,0 +1,123 @@
+using System.Collections;
+using Enums;
+using Manager;
+using UnityEngine;
+
+/// <summary>
+/// Manages the Action Area Shader, adjusting properties when Emojis interact with the area.
+/// </summary>
+public class ActionAreaShader : MonoBehaviour
+{
+    [Tooltip("Material to be manipulated.")]
+    private Material _material;
+
+    // Shader property IDs for efficient access
+    private int _originShaderID;
+    private int _bottomColorShaderID;
+
+    [Header("Height of color ramp")] [SerializeField]
+    private float ShaderRampDuration = 0.2f;
+
+    [SerializeField] private float LowGradientPosition = -0.3f;
+    [SerializeField] private float HighGradientPosition = 0f;
+
+    [Header("Color of color ramp")] [SerializeField]
+    private float TimePressureColorRampDuration = 3;
+
+    [SerializeField] private float ResetColorRampDuration = 0.5f;
+    [SerializeField] private Color TimePressureColor = new(1f, 0.2f, 0f);
+    [SerializeField] private Color ResetColor = new(1f, 0.4f, 0f);
+
+    /// <summary>
+    /// Initialization function, sets up the material and shader property IDs.
+    /// </summary>
+    private void Start()
+    {
+        // Fetching the material from the Renderer component
+        _material = GetComponent<Renderer>().material;
+
+        // Converting shader property names to IDs for efficient access
+        _originShaderID = Shader.PropertyToID("_Origin");
+        _bottomColorShaderID = Shader.PropertyToID("_BottomColor");
+
+        // Setting initial shader property values
+        _material.SetFloat(_originShaderID, -0.3f);
+        _material.SetColor(_bottomColorShaderID, ResetColor);
+    }
+
+    private void Awake()
+    {
+        EventManager.OnEmoteEnteredArea += OnEmoteEnteredAreaCallback;
+        EventManager.OnEmoteExitedArea += OnEmoteExitedAreaCallback;
+    }
+
+    private void OnDestroy()
+    {
+        EventManager.OnEmoteEnteredArea -= OnEmoteEnteredAreaCallback;
+        EventManager.OnEmoteExitedArea -= OnEmoteExitedAreaCallback;
+    }
+
+    /// <summary>
+    /// Callback for when an emoji enters the area.
+    /// </summary>
+    private void OnEmoteEnteredAreaCallback(EEmote emote)
+    {
+        // Stopping all ongoing coroutines to prevent interference
+        StopAllCoroutines();
+
+        // Starting shader property adjustments for emoji entry
+        StartCoroutine(RampShader(HighGradientPosition));
+        StartCoroutine(RampColor(TimePressureColor, TimePressureColorRampDuration));
+    }
+
+    /// <summary>
+    /// Callback for when an emoji exits the area.
+    /// </summary>
+    private void OnEmoteExitedAreaCallback(EEmote emote)
+    {
+        // Stopping all ongoing coroutines to prevent interference
+        StopAllCoroutines();
+
+        // Starting shader property adjustments for emoji exit
+        StartCoroutine(RampShader(LowGradientPosition));
+        StartCoroutine(RampColor(ResetColor, ResetColorRampDuration));
+    }
+
+
+    /// <summary>
+    /// Gradually adjusts a shader property over time. This moves the shader up or down.
+    /// </summary>
+    /// <param name="end">The target value for the shader property.</param>
+    private IEnumerator RampShader(float end)
+    {
+        float time = 0;
+        float start = _material.GetFloat(_originShaderID);
+
+        // Gradually adjusting the shader property over the specified duration
+        while (time < ShaderRampDuration)
+        {
+            _material.SetFloat(_originShaderID, Mathf.SmoothStep(start, end, time / ShaderRampDuration));
+            time += Time.deltaTime;
+            yield return null;
+        }
+    }
+
+    /// <summary>
+    /// Gradually adjusts a color property of the material over time.
+    /// </summary>
+    /// <param name="end">The target color.</param>
+    /// <param name="duration">The duration over which the color change should occur.</param>
+    private IEnumerator RampColor(Color end, float duration)
+    {
+        float time = 0;
+        Color start = _material.GetColor(_bottomColorShaderID);
+
+        // Gradually adjusting the color property over the specified duration
+        while (time < duration)
+        {
+            _material.SetColor(_bottomColorShaderID, Color.Lerp(start, end, time / duration));
+            time += Time.deltaTime;
+            yield return null;
+        }
+    }
+}
