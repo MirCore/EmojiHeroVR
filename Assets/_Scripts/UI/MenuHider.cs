4e03ee9963305d8e0a326a20b78f75ff875bc6a2
MirCore <9919366+MirCore@users.noreply.github.com>
2024-02-06 12:46:55 +0100

    Moved files

--- Assets/_Scripts/UI/MenuHider.cs
+++ Assets/_Scripts/UI/MenuHider.cs
@@ -1,81 +1,84 @@
 ﻿using Manager;
 using UnityEngine;
 
-public class MenuHider : MonoBehaviour
+namespace UI
 {
-    [Header("Hide")]
-    [SerializeField] private bool HideOnLevelStarted;
-    [SerializeField] private bool HideOnLevelFinished;
-    [SerializeField] private bool HideOnGameStarted;
-    [SerializeField] private bool HideOnGameStopped;
-    
-    [Header("Show")]
-    [SerializeField] private bool ShowOnLevelStarted;
-    [SerializeField] private bool ShowOnLevelFinished;
-    [SerializeField] private bool ShowOnGameStarted;
-    [SerializeField] private bool ShowOnGameStopped;
-
-    private GameObject _ui;
-    
-    private void Awake()
+    public class MenuHider : MonoBehaviour
     {
-        EventManager.OnLevelStarted += OnLevelStartedCallback;
-        EventManager.OnGameStarted += GameStartedCallback;
-        EventManager.OnGameStopped += GameStoppedCallback;
-        EventManager.OnLevelFinished += OnLevelFinishedCallback;
+        [Header("Hide")]
+        [SerializeField] private bool HideOnLevelStarted;
+        [SerializeField] private bool HideOnLevelFinished;
+        [SerializeField] private bool HideOnGameStarted;
+        [SerializeField] private bool HideOnGameStopped;
+    
+        [Header("Show")]
+        [SerializeField] private bool ShowOnLevelStarted;
+        [SerializeField] private bool ShowOnLevelFinished;
+        [SerializeField] private bool ShowOnGameStarted;
+        [SerializeField] private bool ShowOnGameStopped;
 
-        _ui = transform.GetChild(0).gameObject;
+        private GameObject _ui;
+    
+        private void Awake()
+        {
+            EventManager.OnLevelStarted += OnLevelStartedCallback;
+            EventManager.OnGameStarted += GameStartedCallback;
+            EventManager.OnGameStopped += GameStoppedCallback;
+            EventManager.OnLevelFinished += OnLevelFinishedCallback;
+
+            _ui = transform.GetChild(0).gameObject;
         
-        HideMenu();
-    }
-
-    private void OnDestroy()
-    {
-        EventManager.OnLevelStarted -= OnLevelStartedCallback;
-        EventManager.OnGameStarted -= GameStartedCallback;
-        EventManager.OnGameStopped -= GameStoppedCallback;
-        EventManager.OnLevelFinished -= OnLevelFinishedCallback;
-    }
-
-    private void OnLevelStartedCallback()
-    {
-        if (HideOnLevelStarted)
             HideMenu();
-        if (ShowOnLevelStarted)
-            ShowMenu();
-    }
+        }
 
-    private void GameStartedCallback()
-    {
-        if (HideOnGameStarted)
-            HideMenu();
-        if (ShowOnGameStarted)
-            ShowMenu();
-    }
+        private void OnDestroy()
+        {
+            EventManager.OnLevelStarted -= OnLevelStartedCallback;
+            EventManager.OnGameStarted -= GameStartedCallback;
+            EventManager.OnGameStopped -= GameStoppedCallback;
+            EventManager.OnLevelFinished -= OnLevelFinishedCallback;
+        }
 
-    private void GameStoppedCallback()
-    {
-        if (HideOnGameStopped)
-            HideMenu();
-        if (ShowOnGameStopped)
-            ShowMenu();
-    }
+        private void OnLevelStartedCallback()
+        {
+            if (HideOnLevelStarted)
+                HideMenu();
+            if (ShowOnLevelStarted)
+                ShowMenu();
+        }
 
-    private void OnLevelFinishedCallback()
-    {
-        if (HideOnLevelFinished)
-            HideMenu();
-        if (ShowOnLevelFinished)
-            ShowMenu();
-    }
+        private void GameStartedCallback()
+        {
+            if (HideOnGameStarted)
+                HideMenu();
+            if (ShowOnGameStarted)
+                ShowMenu();
+        }
 
-    private void HideMenu()
-    {
-        _ui.SetActive(false);
-    }
+        private void GameStoppedCallback()
+        {
+            if (HideOnGameStopped)
+                HideMenu();
+            if (ShowOnGameStopped)
+                ShowMenu();
+        }
 
-    private void ShowMenu()
-    {
-        _ui.SetActive(true);
+        private void OnLevelFinishedCallback()
+        {
+            if (HideOnLevelFinished)
+                HideMenu();
+            if (ShowOnLevelFinished)
+                ShowMenu();
+        }
+
+        private void HideMenu()
+        {
+            _ui.SetActive(false);
+        }
+
+        private void ShowMenu()
+        {
+            _ui.SetActive(true);
+        }
     }
 }
